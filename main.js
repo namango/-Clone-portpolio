53134b52e8f1455cb8b4cce39dfb4e5326701515
Min <63222032+namango@users.noreply.github.com>
2021-05-12 18:50:52 +0900

    JS: Scorll

---
+++ main.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  const value = document.querySelector(link);
+  if (link == null) {
+    return;
+  }
+  value.scrollIntoView({ behavior: "smooth", block: "start" });
+});
+
+// Handel scrolling when click the contack button
+const contactBtn = document.querySelector(".home__contact");
+contactBtn.addEventListener("click", () => {
+  document.querySelector("#contact").scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+});
