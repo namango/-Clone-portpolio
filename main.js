07adbacacb4d21d1ca4f4bbad2c3804d76695127
Min <63222032+namango@users.noreply.github.com>
2021-05-12 23:30:06 +0900

    JS: project Btn(selected)

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ navbarMenu.addEventListener("click", (event) => {
   const target = event.target;
   const link = target.dataset.link;
   const value = document.querySelector(link);
+
   if (link == null) {
     return;
   }
@@ -66,11 +67,24 @@ categoryBtn.addEventListener("click", (event) => {
   if (filter == null) {
     return;
   }
-  projects.forEach((project) => {
-    if (filter === "*" || filter === project.dataset.type) {
-      project.classList.remove("invisible");
-    } else {
-      project.classList.add("invisible");
-    }
-  });
+
+  //   Remove previous selection and select the new one
+  const active = document.querySelector(".category__btn.selected");
+  active.classList.remove("selected");
+  const target =
+    event.target.nodeName === "BUTTON" ? event.target : event.target.parentNode;
+  target.classList.add("selected");
+
+  projectContainer.classList.add("anim-out");
+
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (filter === "*" || filter === project.dataset.type) {
+        project.classList.remove("invisible");
+      } else {
+        project.classList.add("invisible");
+      }
+    });
+    projectContainer.classList.remove("anim-out");
+  }, 300);
 });
