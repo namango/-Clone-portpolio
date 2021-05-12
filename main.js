4ab3a18fadc7eac6a1a756262d22bb467343948d
Min <63222032+namango@users.noreply.github.com>
2021-05-12 22:34:23 +0900

    JS: project_invisible

--- main.js
+++ main.js
@@ -53,3 +53,24 @@ document.addEventListener("scroll", () => {
 arrowBtn.addEventListener("click", () => {
   document.querySelector("#home").scrollIntoView({ behavior: "smooth" });
 });
+
+// Projects
+
+const categoryBtn = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+
+categoryBtn.addEventListener("click", (event) => {
+  const filter =
+    event.target.dataset.filter || event.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+  projects.forEach((project) => {
+    if (filter === "*" || filter === project.dataset.type) {
+      project.classList.remove("invisible");
+    } else {
+      project.classList.add("invisible");
+    }
+  });
+});
